3c26afe1f0707d539fe709e703c8d964ea207e05
kishan kumar <kishan5183751@gmail.com>
2025-08-31 18:48:22 +0530

    completed user backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
+import ownerRouter from "./routes/ownerRoutes.js";
 
 const app = express();
+
+await connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +15,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running..");
 });
+
+app.use("/api/user", userRouter);
+app.use("/api/owner", ownerRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
