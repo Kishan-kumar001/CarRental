2eace38435075a16f2f23ce46c4d8460a1c9b523
kishan kumar <kishan5183751@gmail.com>
2025-09-01 21:40:20 +0530

    Completed Add car

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,10 +1,42 @@
+import imagekit from "../configs/imageKit.js";
+import Car from "../models/Car.js";
 import User from "../models/User.js";
+import fs from "fs";
 
 export const changeRoleToOwner = async (req, res) => {
   try {
     const { _id } = req.user;
     await User.findByIdAndUpdate(_id, { role: "owner" });
     res.json({ success: true, message: "Now you can list cars" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const addCar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    let car = JSON.parse(req.body.carData);
+    const imageFile = req.file;
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/cars",
+    });
+    // For URL Generation, works for both images and videos
+    var optimizedImageURL = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { width: "1280" },
+        { quality: "auto" },
+        { format: "webp" },
+      ],
+    });
+    const image = optimizedImageURL;
+    await Car.create({ ...car, owner: _id, image });
+    res.status(201).json({ success: true, message: "Car Added" });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
